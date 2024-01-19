6c3e7a5e6e46ef4cfca8d6a01c8bb0c0f5039b5e
Ademilton Mariano <ademilton.mariano@ravex.com.br>
2024-01-19 08:46:39 -0300

    Iniciando o projeto

---
+++ xadrez_curso/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Tabuleiro;
+
+Posicao Posicao;
+Tabuleiro.Tabuleiro Tabuleiro;
+
+Tabuleiro = new Tabuleiro.Tabuleiro(8, 8);
+
+Console.ReadLine();
---
+++ xadrez_curso/Tabuleiro/Peca.cs
@@ -0,0 +1,17 @@
+﻿namespace Tabuleiro;
+
+public class Peca
+{
+    public Posicao Posicao { get; set; }
+    public Cor Cor { get; set; }
+    public int QuantidadeMovimentos { get; protected set; }
+    public Tabuleiro Tabuleiro { get; protected set; }
+
+    public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
+    {
+        Posicao = posicao;
+        Cor = cor;
+        Tabuleiro = tabuleiro;
+        QuantidadeMovimentos = 0;
+    }
+}
