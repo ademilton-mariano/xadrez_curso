08ed442e22ce3d771549a2337882e4586d66f278
Ademilton Mariano <ademilton.mariano@ravex.com.br>
2024-01-19 09:00:49 -0300

    Imprimindo tabuleiro

--- xadrez_curso/Program.cs
+++ xadrez_curso/Program.cs
@@ -1,10 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Tabuleiro;
+using xadrez_curso;
 
 Posicao Posicao;
 Tabuleiro.Tabuleiro Tabuleiro;
 
 Tabuleiro = new Tabuleiro.Tabuleiro(8, 8);
+
+Tela.ImprimirTabuleiro(Tabuleiro);
 
 Console.ReadLine();
